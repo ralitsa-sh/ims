6f0c9b00a3dc473730587db02cf488b5c6b2fafe
Rasmus Hammar <rasmus.hammar@gmx.com>
2026-09-11 11:16:19 +0200

    Add test PHP files for basic echo and print testing

---
+++ test.php
@@ -0,0 +1,16 @@
+
+<html>
+    <body>
+        <p>Test PHP</p>
+<?php
+// phpinfo()
+
+$my_text="Hello, World!<Br>";
+print_r($my_text);
+echo $my_text;
+
+echo "What's up, World?<Br>";
+?>
+
+</body>
+</html>
