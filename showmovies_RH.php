ef1261a82786120ba1a140869ce7e5585ad4641c
Rasmus Hammar <rasmus.hammar@gmx.com>
2026-09-16 15:50:42 +0200

    Add movie search functionality with name-based filtering

--- showmovies_RH.php
+++ showmovies_RH.php
@@ -6,12 +6,29 @@
 <!-- Link back to index.php -->
 <a href="index_RH.php">Movie Submission Form</a>
 
+<!-- Form for movie name seach, reloads the page -->
+<form action="/showmovies_RH.php" method="post">
+    <label for="mname_search">Search by movie name:</label><br>
+    <input type="text" id="mname_search" name="mname_search"><br><br>
+    <input type="submit" value="Search">
+</form>
+
+<!-- If search is submitted, use it to filter the results -->
+<?php
+if (isset($_POST['mname_search'])) {
+    $search_term = "%" . $_POST['mname_search'] . "%"; // Use wildcard for partial matches
+} else {
+    $search_term = '%'; // Default to all movies if no search term is provided
+}
+?>
+
 <!-- Fetch movies from database -->
  <?php
 include 'db_open_RH.php';
 // Query to select all movies from the movies table
-$sql_movies = "SELECT mname, myear, mgenre, mrating FROM movies";
+$sql_movies = "SELECT mname, myear, mgenre, mrating FROM movies WHERE mname LIKE ?";
 $stmt_movies = $conn->prepare($sql_movies);
+$stmt_movies->bind_param("s", $search_term);
 $result_movies = $stmt_movies->execute();
  ?>
 
